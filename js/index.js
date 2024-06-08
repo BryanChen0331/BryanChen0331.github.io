58476ad6bba83176cd4261412548304c225c4452
BryanChen0331 <111680145+BryanChen0331@users.noreply.github.com>
2024-06-09 07:58:16 +0800

    Add files via upload
    v3.0 stable

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
-import { questions } from './questions.js';
+import {questions} from "./questions.js";
 
-let currentQuestion = -1;
+let currentQuestion = 0;
+let userName;
+let handleVideoEndedWrapper;
 
 const attributes = {
     adventure: 0,
@@ -11,104 +13,186 @@ const attributes = {
     intuition: 0
 };
 
-function generateCharacter(attributes) {
+function generateCharacter(attributes){
     const attributeToCharacter = {
-        adventure: "DJ章魚",
-        social: "人騎海馬",
-        creativity: "健力龍蝦",
-        strategy: "河豚",
-        emotion: "墨鏡海豚",
-        intuition: "水母"
+        adventure: "/src/result1.png",
+        social: "/src/result2.png",
+        creativity: "/src/result3.png",
+        strategy: "/src/result4.png",
+        emotion: "/src/result5.png",
+        intuition: "/src/result6.png"
     };
 
     const max_value = Math.max(...Object.values(attributes));
     const highest_attributes = Object.keys(attributes).filter(attr => attributes[attr] === max_value);
     const selected_attribute = highest_attributes[Math.floor(Math.random() * highest_attributes.length)];
-    const character = attributeToCharacter[selected_attribute];
+    const characterImg = attributeToCharacter[selected_attribute];
     
-    return character;
+    return characterImg;
 }
 
 window.onload = function(){
-    const $title = document.querySelector("#title");
-    const $contentBox = document.querySelector("#content-box");
-    const $content = document.querySelector("#content");
-    const $character = document.querySelector("#character");
-    const $startBtn = document.querySelector("#start-btn");
+    const $bg = document.querySelector("#bg");
+    const $result = document.querySelector("#result");
+    const $userName = document.querySelector("#username");
+    const $mask = document.querySelector("#mask")
+    const $cutscene = document.querySelector("#cutscene");
+    const $cutsceneBg = document.querySelector("#cutscene-bg");
+    const $text = document.querySelector("#text");
+    const $inputContainer = document.querySelector("#input-container");
+    const $inputBox = document.querySelector("#input-box");
+    const $skipBtn = document.querySelector("#skip-btn");
     const $btn1 = document.querySelector("#btn1");
     const $btn2 = document.querySelector("#btn2");
     const $btn3 = document.querySelector("#btn3");
     const $btn4 = document.querySelector("#btn4");
-    const $restartBtn = document.querySelector("#restart-btn");
+    const $btn5 = document.querySelector("#btn5");
+    const $questionContainer = document.querySelector("#question-container");
+    const $question = document.querySelector("#question");
+    const $opBtn2 = document.querySelector("#op-btn2");
+    const $opBtn1 = document.querySelector("#op-btn1");
+    const $opBtn3 = document.querySelector("#op-btn3");
+    const $opBtn4 = document.querySelector("#op-btn4");
 
-    function start(){
-        $title.style.display = "none";
-        $startBtn.style.display = "none";
-
-        $contentBox.style.display = "block";
-        $content.style.display = "block";
-        $btn1.style.display = "block";
-        $btn2.style.display = "block";
-        $btn3.style.display = "block";
-
-        update();
+    function toggleVisibility(element){
+        element.classList.toggle("hidden");
+    }
+    
+    function toNextPage(callback){
+        function handleFirstAnimationEndWrapper() {
+            $cutsceneBg.removeEventListener("animationend", handleFirstAnimationEndWrapper);
+            $cutsceneBg.addEventListener('animationend', handleSecondAnimationEndWrapper);
+            $cutsceneBg.classList.toggle("fadeInToBlack");
+            $cutsceneBg.classList.toggle("fadeOutFromBlack");
+    
+            callback();
+        }
+    
+        function handleSecondAnimationEndWrapper() {
+            $cutsceneBg.removeEventListener("animationend", handleSecondAnimationEndWrapper);
+            $cutsceneBg.classList.toggle("fadeOutFromBlack");
+            toggleVisibility($cutscene);
+        }
+    
+        $cutsceneBg.addEventListener("animationend", handleFirstAnimationEndWrapper);
+        $cutsceneBg.classList.toggle("fadeInToBlack");
+        toggleVisibility($cutscene);
+    }
+    
+    function fn1(){
+        toNextPage(() => {
+            $bg.src = "/src/bg2.mp4";
+            toggleVisibility($btn1);
+            toggleVisibility($text);
+            toggleVisibility($inputContainer);
+            toggleVisibility($btn2);
+        });
     }
 
-    function update(option){
-        $btn4.style.display = "none";
+    function fn2(){
+        userName = $inputBox.value;
+        if (userName){
+            toNextPage(() => {
+                $bg.src = "/src/bg3.mp4";
+                $bg.loop = false;
+                toggleVisibility($text);
+                toggleVisibility($inputContainer);
+                toggleVisibility($skipBtn);
+                toggleVisibility($btn2);
 
-        if (currentQuestion < 8 && option !== undefined){
+                handleVideoEndedWrapper = function() {
+                    fn3();
+                };
+
+                $bg.addEventListener("ended", handleVideoEndedWrapper);
+            });
+        }
+    }
+
+    function fn3(){
+        toNextPage(() => {
+            $bg.removeEventListener("ended", handleVideoEndedWrapper);
+
+            if (handleVideoEndedWrapper) {
+                $bg.removeEventListener("ended", handleVideoEndedWrapper);
+            }
+
+            $bg.src = "/src/bg4.mp4";
+            $bg.loop = true;
+            toggleVisibility($mask);
+            toggleVisibility($skipBtn);
+
+            $question.innerText = questions[0].question;
+            $opBtn1.innerText = questions[0].options[0].text;
+            $opBtn2.innerText = questions[0].options[1].text;
+            $opBtn3.innerText = questions[0].options[2].text;
+            $questionContainer.classList.toggle("question-container-layout1");
+            toggleVisibility($questionContainer);
+        });
+    }
+
+    function nextQuestion(option){
+        toNextPage(() => {
+            $opBtn4.style.display = "none";
+
             const {effect} = questions[currentQuestion].options[option];
             attributes[effect]++;
-        }
+            currentQuestion++;
+            $bg.src = `/src/bg${currentQuestion+4}.mp4`;
 
-        currentQuestion++;
-        
-        if (currentQuestion < 8){
-            const {question} = questions[currentQuestion];
-            const options = questions[currentQuestion].options.map(option => option.text);
-            
-            $content.innerText = question;
-            $btn1.innerText = options[0];
-            $btn2.innerText = options[1];
-            $btn3.innerText = options[2];
-            
-            if (currentQuestion === 4){
-                $btn4.innerText = options[3];
-                $btn4.style.display = "block";
+            if (currentQuestion < 8){
+                $question.innerText = questions[currentQuestion].question;
+                $opBtn1.innerText = questions[currentQuestion].options[0].text;
+                $opBtn2.innerText = questions[currentQuestion].options[1].text;
+                $opBtn3.innerText = questions[currentQuestion].options[2].text;
+            } else {
+                toggleVisibility($mask);
+                toggleVisibility($questionContainer);
+                toggleVisibility($btn3);
             }
-        } else {
-            showResult();
-        }
+
+            if (currentQuestion === 4){
+                $opBtn4.innerText = questions[currentQuestion].options[3].text;
+                $opBtn4.style.display = "block";
+            }
+
+            if (currentQuestion === 1){
+                $mask.src = "/src/mask2.png";
+                $questionContainer.classList.toggle("question-container-layout1");
+                $questionContainer.classList.toggle("question-container-layout2");
+            }
+            if (currentQuestion === 3){
+                $mask.src = "/src/mask1.png";
+                $questionContainer.classList.toggle("question-container-layout1");
+                $questionContainer.classList.toggle("question-container-layout2");
+            }
+        });
     }
 
-    function showResult(){
-        $content.style.display = "none";
-        $btn1.style.display = "none";
-        $btn2.style.display = "none";
-        $btn3.style.display = "none";
+    function fn4(){
+        const characterImg = generateCharacter(attributes);
+        $result.src = characterImg;
+        $userName.innerText = userName;
 
-        const character = generateCharacter(attributes)
-        $character.style.display = "block";
-        $character.innerText = character;
-
-        $restartBtn.style.display = "block";
+        toNextPage(() => {
+            toggleVisibility($bg);
+            toggleVisibility($result);
+            toggleVisibility($userName);
+            toggleVisibility($btn3);
+            toggleVisibility($btn4);
+            toggleVisibility($btn5);
+        });
     }
 
-    $startBtn.addEventListener("click", start);
-    $btn1.addEventListener("click", () => {
-        update(0)
+    $skipBtn.addEventListener("click", () => {
+        fn3();
     });
-    $btn2.addEventListener("click", () => {
-        update(1)
-    });
-    $btn3.addEventListener("click", () => {
-        update(2)
-    });
-    $btn4.addEventListener("click", () => {
-        update(3)
-    });
-    $restartBtn.addEventListener("click", () => {
-        window.location.reload();
-    });
+    $btn1.addEventListener("click", fn1);
+    $btn2.addEventListener("click", fn2);
+    $btn3.addEventListener("click", fn4);
+    $btn4.addEventListener("click", () => window.location.reload());
+    $opBtn1.addEventListener("click", () => nextQuestion(0));
+    $opBtn2.addEventListener("click", () => nextQuestion(1));
+    $opBtn3.addEventListener("click", () => nextQuestion(2));
+    $opBtn4.addEventListener("click", () => nextQuestion(3));
 }
